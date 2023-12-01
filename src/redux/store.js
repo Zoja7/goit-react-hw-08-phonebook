fb45de4f8e9aad7606fdf3ab7fab07f402b2ecc6
Zoja7 <yaozonka@gmail.com>
2023-12-01 17:01:40 +0200

    commit_1

---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// const productsConfig = {
+//   key: '',
+//   storage,
+//   whitelist: [''],
+// };
+
+// const authConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
